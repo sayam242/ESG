e1014ed09809ea04089d2543d4b9708708b6dbb7
Suminder <sumi786nyol@gmail.com>
2026-07-12 12:04:10 +0530

    feat(middleware): add authentication middleware for token verification

---
+++ backend/src/shared/middlewares/auth.middleware.js
@@ -0,0 +1,21 @@
+import "dotenv/config";
+
+const jwt = require("jsonwebtoken");
+
+const authMiddleware = (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ message: "Access denied." });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res.status(400).json({ message: "Invalid token." });
+  }
+};
+
+export default authMiddleware;
